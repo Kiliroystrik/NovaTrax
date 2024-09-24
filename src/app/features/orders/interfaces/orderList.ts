34b80cc6e37b597055d529d1b8a61a839d781569
Kiliroy <borges.mathieu@gmail.com>
2024-09-24 14:35:26 +0200

    Minor fixes

--- src/app/features/orders/interfaces/orderList.ts
+++ src/app/features/orders/interfaces/orderList.ts
@@ -3,8 +3,13 @@ export interface Order {
     orderNumber: string | null; // N° de commande
     createdAt: string | null; // Date de création (string au format ISO 8601)
     updatedAt: string | null; // Date de mise à jour (nullable)
-    customerName: string | null; // Nom du client
+    client: Client; // Nom du client
     orderDate: string | null; // Date de la commande (string au format ISO 8601)
     expectedDeliveryDate: string | null; // Date de livraison prévue (string au format ISO 8601)
     status: string | null; // Statut de la commande
+}
+
+export interface Client {
+    id: number | null; // Identifiant unique
+    name: string | null; // Nom du client
 }
